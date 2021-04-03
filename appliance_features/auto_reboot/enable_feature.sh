196f02e03d3ed29b1041f9ebcba37400ae363be3
Todd Esposito <todd@toddesposito.com>
2021-04-02 22:07:18 -0500

    refactor auto_reboot naming

--- appliance_features/auto_reboot/enable_feature.sh
+++ appliance_features/auto_reboot/enable_feature.sh
@@ -2,11 +2,11 @@
 #%% Auto-Reboot the Appliance
 #%@ reboot_hour Reboot at Hour (UTC)
 #%@ reboot_day Reboot on day of the week (0-6 or SUN-SAT)
-#%< auto-reboot.cron
+#%< auto_reboot.cron
 
 echo -e 'Enabling Auto-Reboot...'
 tab="/var/spool/cron/crontabs/pi"
 sudo touch $tab
 sudo chmod 600 $tab
 sudo chown pi:crontab $tab
-sudo bash -c "cat </home/pi/.templatefiles/auto-reboot/auto-reboot.cron >>$tab"
+sudo bash -c "cat </home/pi/.templatefiles/auto_reboot/auto_reboot.cron >>$tab"
